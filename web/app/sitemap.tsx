ddaa06b02697e2222c4484be0e36b72e7407731a
Gokulasigamani <gokulsusela@gmail.com>
2025-10-30 13:24:06 +0530

    sitemap added

---
+++ web/app/sitemap.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import type { MetadataRoute } from 'next'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = "https://sunajo.com";
+    const now = new Date().toISOString();
+    return [
+        { url: "/", changefreq: "daily", priority: 1.0, lastModified: now },
+        { url: "/about", changefreq: "monthly", priority: 0.9, lastModified: now },
+        { url: "/services", changefreq: "monthly", priority: 0.9, lastModified: now },
+        { url: "/capabilities", changefreq: "monthly", priority: 0.8, lastModified: now },
+        { url: "/products", changefreq: "monthly", priority: 0.8, lastModified: now },
+        { url: "/careers", changefreq: "monthly", priority: 0.7, lastModified: now },
+        { url: "/contact", changefreq: "yearly", priority: 0.7, lastModified: now },
+        { url: "/calendly", changefreq: "yearly", priority: 0.6, lastModified: now },
+        { url: "/privacy", changefreq: "yearly", priority: 0.5, lastModified: now },
+        { url: "/terms", changefreq: "yearly", priority: 0.5, lastModified: now },
+    ].map(route => ({
+        ...route,
+        url: baseUrl + route.url,
+    }))
+}
